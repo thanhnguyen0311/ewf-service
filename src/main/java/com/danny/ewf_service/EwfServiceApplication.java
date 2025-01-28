a58ee4bd0fc2365b222e374ab21c1bcad05b953b
DESKTOP-4JH7URA\Thanh <nct031194@icloud.com>
2025-01-28 02:25:14 -0500

    init

---
+++ src/main/java/com/danny/ewf_service/EwfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.danny.ewf_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwfServiceApplication.class, args);
+	}
+
+}
