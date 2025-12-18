68d0a30c8d1e789c31cddcc8c162df1f0b136c2c
THANHPC\Accou <nct031194@icloud.com>
2025-12-18 16:49:52 -0600

    Refactor active campaign query in repository and service.
    Replaced repository method with a custom @Query to include an additional filter for non-B2B campaigns. Updated the service to use the new repository method for fetching active campaigns.

--- src/main/java/com/danny/ewf_service/repository/WayfairCampaignParentSkuRepository.java
+++ src/main/java/com/danny/ewf_service/repository/WayfairCampaignParentSkuRepository.java
@@ -2,12 +2,14 @@ package com.danny.ewf_service.repository;
 
 import com.danny.ewf_service.entity.wayfair.WayfairCampaignParentSku;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface WayfairCampaignParentSkuRepository extends JpaRepository<WayfairCampaignParentSku, Long> {
+    @Query("SELECT wcps FROM WayfairCampaignParentSku wcps WHERE wcps.campaign.isActive = true AND wcps.campaign.isB2b = false")
+    List<WayfairCampaignParentSku> findAllCampaign();
 
-    List<WayfairCampaignParentSku> findAllByCampaignIsActiveIsTrue();
 }
